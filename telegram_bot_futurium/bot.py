426e4a72123f2829f709026297fcaaa9769df127
nvkoval <nvkoval@gmail.com>
2022-12-25 18:05:04 +0200

    test aiogram2

---
+++ telegram_bot_futurium/bot.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+# from aiogram import Command
+from config_reader import config
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=config.bot_token.get_secret_value())
+
+dp = Dispatcher(bot)
+
+# Hendler for /start command
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer("Hey! \
+Вітаємо у Futurium English School боті - \
+твоєму помічнику на шляху у вивченні англійської💛 \
+\nЩоб ми могли вам краще допомогти - \
+підкажіть нам ваш статус у школі ⬇️")
+
+# Polling new updates
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
